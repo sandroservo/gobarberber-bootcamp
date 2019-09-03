d215509d9f4c642a5ee271202b2b05d811dceb7e
sandroservo <sandro_servo@hotmail.com>
2019-09-02 22:34:45 -0300

    chore: gobarber não  redireciona

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatorio'),
+  password: Yup.string().required('A senha é Obrigatoria'),
+});
+
+export default function SignIn() {
+  const dispath = useDispatch();
+
+  function handleSubmit(email, password) {
+    dispath(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
+}
